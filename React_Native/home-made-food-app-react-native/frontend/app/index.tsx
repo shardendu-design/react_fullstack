281fcd01f8806bb1172df5b395dd6dd22b3e2e8b
shardendu <appujha0@gmail.com>
2025-02-09 17:21:15 +0200

    (hmfrn): reset-project

---
+++ React_Native/home-made-food-app-react-native/frontend/app/index.tsx
@@ -0,0 +1,32 @@
+// import { Text, View } from "react-native";
+
+// export default function Index() {
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+//       <Text>Edit app/index.tsx to edit this screen.</Text>
+//     </View>
+//   );
+// }
+
+
+import { Text, View } from "react-native";
+
+const Index = () => {
+  return (
+    <View>
+      <Text>
+        Hello
+
+      </Text>
+    </View>
+  );
+};
+
+
+export default Index;
